cf4921af6dd3ce9f1cc59cea5c23196438aae3cd
Mehdi-MI <128828181+Mehdi-MI@users.noreply.github.com>
2023-06-06 16:56:23 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tic_tac_toe/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'TIC TAC TOE',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/widgets/cubes.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:tic_tac_toe/constants/constant.dart';
+import 'package:tic_tac_toe/controllers/controlle.dart';
+import 'package:tic_tac_toe/utils/utils.dart';
+import '../utils/config.dart';
+
+class CubesWidget extends StatelessWidget {
+  final String displayElement;
+  final int i, j;
+  final TicTacToeController ticTacToeController =
+      Get.put(TicTacToeController());
+  CubesWidget(
+      {Key? key,
+      required this.displayElement,
+      required this.i,
+      required this.j})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return GetBuilder<TicTacToeController>(builder: (_) {
+      return IgnorePointer(
+        ignoring: ticTacToeController.stopInteraction.value,
+        child: Container(
+          width: SizeConfig.safeBlockHorizontal * 26,
+          height: SizeConfig.safeBlockVertical * 12,
+          decoration: BoxDecoration(
+              color: Constants.backgroundColor,
+              border: Util.getBorderSide(i, j)),
+          child: Center(
+              child: Text(
+            displayElement,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 80,
+                color: displayElement == 'X'
+                    ? Constants.whiteColor
+                    : Constants.accentColor),
+          )),
+        ),
+      );
+    });
+  }
+}
